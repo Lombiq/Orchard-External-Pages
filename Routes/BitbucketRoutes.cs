9bfc7cd8f8e6f25fb964dd3abff4cc66599dda36
Piedone <piedone@pyrocenter.hu>
2013-03-18 23:00:51 +0100

    Rerouting BB admin controller to be under the path Admin/

---
+++ Routes/BitbucketRoutes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Environment.Extensions;
+using Orchard.Mvc.Routes;
+
+namespace OrchardHUN.ExternalPages.Routes
+{
+    [OrchardFeature("OrchardHUN.ExternalPages.Bitbucket")]
+    public class BitbucketRoutes : IRouteProvider
+    {
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[]
+            {
+                new RouteDescriptor
+                {
+                    Name = "BitbucketAdminRoute",
+                    Route = new Route(
+                        "Admin/OrchardHUN.ExternalPages/Bitbucket/{action}",
+                        new RouteValueDictionary
+                        {
+                            {"area", "OrchardHUN.ExternalPages"},
+                            {"controller", "BitbucketAdmin"}
+                        },
+                        new RouteValueDictionary(),
+                        new RouteValueDictionary
+                        {
+                            {"area", "OrchardHUN.ExternalPages"}
+                        },
+                        new MvcRouteHandler())
+                }
+            };
+        }
+    }
+}
